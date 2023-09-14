f5c872af125fe0773b55305df81d3d8cf1e2e998
KaaVii <daniloventurini2112@gmail.com>
2023-09-14 14:17:32 -0300

    Second Commit - Uh boy... the time goes by

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    );
+  await app.listen(process.env.PORT || 3000);
 }
+config();
 bootstrap();
